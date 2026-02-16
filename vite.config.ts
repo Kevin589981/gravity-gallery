252a8aa520f5dac5344d70d8cb7889c43e69d5f9
Kevin589981 <3627713050@qq.com>
2026-02-16 12:35:48 +0800

    solve兼容性

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => ({
   server: {
-    port: 3000,
+    port: 4861,
     host: '0.0.0.0',
     https: {
       key: fs.readFileSync(
